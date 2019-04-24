8e2fd45b8e510692fee7bf8ef7af6476b27c8c78
Zeda Thomas <xedaelnara@gmail.com>
2019-04-24 15:49:39 -0400

    Optimize `ex de,hl \ ex de,hl` ==> ``

--- z80opt.py
+++ z80opt.py
@@ -17,6 +17,8 @@ def z80optimizer(src):
     elif src[k].endswith("),hl") and src[k+1].startswith(" ld hl,(") and src[k][5:-3]==src[k+1][8:]:
       s+=[src[k]]
       k+=2
+    elif src[k]==" ex de,hl" and src[k+1]==" ex de,hl":
+      k+=2
     else:
       s+=[src[k]]
       k+=1
